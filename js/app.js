31768931ab4fd98f4a7826366144d93a5305012a
Ferhat KAYMAZ <ferhatkaymaz2023@gmail.com>
2022-02-20 15:59:01 +0300

    showUser

--- js/app.js
+++ js/app.js
@@ -3,9 +3,40 @@ const nameInput = document.getElementById('githubname');
 const clearLastUsers = document.getElementById('clear-last-users');
 const lastUsers = document.getElementById('last-users');
 
+const github = new Github();
+const ui = new UI();
+
 addEventListeners();
 
 function addEventListeners()
 {
-    
+    form.addEventListener('submit',getData);
+}
+
+function getData(e)
+{
+    const username = nameInput.value.trim();
+
+    if(username === '')
+    {
+        console.log('empty github user');
+    }
+    else 
+    {
+        github.getGithubData(username)
+            .then(data => {
+                if(data.user.message === 'Not Found')
+                {
+                    console.log('User not found');
+                }
+                else 
+                {
+                    ui.showUser(data.user);   
+                }
+            })
+    }
+
+
+
+    e.preventDefault();
 }
--- js/github.js
+++ js/github.js
@@ -2,6 +2,20 @@
     {
         constructor()
         {
-            
+            this.url = 'http://api.github.com/users/';
         }
-    }
+
+        async getGithubData(username)
+        {
+            const userResponse = await fetch(this.url+username);
+            const repoResponse = await fetch(this.url+username+'/repos');
+
+            const userData = await userResponse.json();
+            const repoData = await repoResponse.json();
+
+            return {
+                user:userData,
+                repo:repoData
+            };
+        };
+    };
